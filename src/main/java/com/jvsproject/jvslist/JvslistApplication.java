ef74336ee5777063870c7840f0f777241041e73d
Arthur de Sa <arthur.invic@gmail.com>
2025-08-19 00:29:31 -0300

    Creating project

---
+++ src/main/java/com/jvsproject/jvslist/JvslistApplication.java
@@ -0,0 +1,13 @@
+package com.jvsproject.jvslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jvsproject/jvslist/JvslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jvsproject.jvslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
